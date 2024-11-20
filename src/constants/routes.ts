e8f5b6d759c384cff504653bc0e43a508a90a1fc
Julio Quina <julio.quina@blueprism.com>
2024-11-20 08:34:24 +0000

    fix: routing not navigating

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,7 @@
 export namespace AppRoutes {
   export const HomePage = '/';
-  export const Analytics = `${HomePage}/analytics`;
-  export const Pokedex = `${HomePage}/mypokedex`;
+  export const Analytics = `${HomePage}analytics`;
+  export const Pokedex = `${HomePage}mypokedex`;
   export const PokeDexDetails = `${Pokedex}/:pokemonName`;
-  export const PokemonDetails = `${HomePage}/pokemon:pokemonName`;
+  export const PokemonDetails = `${HomePage}/pokemon/:pokemonName`;
 }

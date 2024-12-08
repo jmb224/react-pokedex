f6fd56c62303e52344d47fab5c716f819103e601
Julio Quina <jmb19quina@hotmail.com>
2024-12-08 15:50:08 +0000

    refactor: Move styled components to its own file

---
+++ src/components/SearchBar/SearchResult.styled.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const StyledPokemonName = styled.span`
+  cursor: pointer;
+  background-color: none;
+  display: block;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  transition: all 0.3s ease;
+  font-size: 1rem;
+  color: #333;
+  text-transform: capitalize;
+  font-weight: 500;
+
+  &:hover {
+    color: #7263f3;
+    background-color: #f0f0f0;
+  }
+`;
+
+export const StyledSearchResults = styled.div`
+  position: relative;
+  max-height: 15rem;
+  overflow: auto;
+  z-index: 2;
+  padding: 1rem;
+  border-radius: 0.8rem;
+  background-color: white;
+  box-shadow: 0.2rem 0.25rem 0.9rem rgba(0, 0, 0, 0.2);
+`;
--- src/components/SearchBar/SearchResults.tsx
+++ src/components/SearchBar/SearchResults.tsx
@@ -1,40 +1,11 @@
-import styled from 'styled-components';
 import { useModal } from '../../hooks';
-import { ViewDetailsModal } from '../Modal';
 import { Pokemon } from '../../types';
+import { ViewDetailsModal } from '../Modal';
+import { StyledPokemonName, StyledSearchResults } from './SearchResult.styled';
 
 type SearchResultsProps = {
   searchResults: Pokemon[];
 };
-
-const StyledPokemonName = styled.span`
-  cursor: pointer;
-  background-color: none;
-  display: block;
-  padding: 0.5rem;
-  border-radius: 0.5rem;
-  transition: all 0.3s ease;
-  font-size: 1rem;
-  color: #333;
-  text-transform: capitalize;
-  font-weight: 500;
-
-  &:hover {
-    color: #7263f3;
-    background-color: #f0f0f0;
-  }
-`;
-
-const StyledSearchResults = styled.div`
-  position: relative;
-  max-height: 15rem;
-  overflow: auto;
-  z-index: 2;
-  padding: 1rem;
-  border-radius: 0.8rem;
-  background-color: white;
-  box-shadow: 0.2rem 0.25rem 0.9rem rgba(0, 0, 0, 0.2);
-`;
 
 export function SearchResults({ searchResults }: SearchResultsProps) {
   const { name, showCard, setName, toggleModal } = useModal();
@@ -46,17 +17,15 @@ export function SearchResults({ searchResults }: SearchResultsProps) {
 
   return (
     <>
-      {searchResults.length > 0 && (
-        <StyledSearchResults>
-          {searchResults.map((pokemon) => {
-            return (
-              <StyledPokemonName key={pokemon.name} onClick={() => handleSearchClick(pokemon.name)}>
-                {pokemon.name}
-              </StyledPokemonName>
-            );
-          })}
-        </StyledSearchResults>
-      )}
+      <StyledSearchResults>
+        {searchResults?.map((pokemon) => {
+          return (
+            <StyledPokemonName key={pokemon.name} onClick={() => handleSearchClick(pokemon.name)}>
+              {pokemon.name}
+            </StyledPokemonName>
+          );
+        })}
+      </StyledSearchResults>
       <ViewDetailsModal show={showCard} setShow={toggleModal} pokemonName={name} />
     </>
   );
